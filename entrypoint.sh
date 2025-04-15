6cee6aa2b073f6d8bb18e9bf5b57c866b803ed6d
nguyenthang <Tl291095@>
2025-04-15 11:18:39 +0700

    Customize AVD configuration

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,16 +33,18 @@ set -e
 
 #!/bin/bash
 
-echo "Starting Appium..."
+echo "[1/5] Starting Appium server..."
 appium -a 0.0.0.0 -p 4723 -pa /wd/hub --allow-cors --relaxed-security > appium_log.txt 2>&1 &
 sleep 5
 
 
-# Your original script here
+echo "[2/5] Starting Android Emulator..."
 "$ANDROID_HOME"/emulator/emulator -avd emu -no-audio -no-window -gpu swiftshader_indirect -no-snapshot -no-boot-anim -verbose &
 
+echo "[3/5] Waiting for Emulator to boot..."
 sleep 30
 
+echo "[4/5] Disabling hidden APIs and animations..."
 adb shell settings put global window_animation_scale 0.0
 adb shell settings put global transition_animation_scale 0.0
 adb shell settings put global animator_duration_scale 0.0
@@ -50,5 +52,5 @@ adb shell settings put global animator_duration_scale 0.0
 adb devices
 
 # Keep the container session active
-echo "Container is running. Opening a shell..."
+echo "[5/5] All set. Emulator & Appium are ready. Keeping container alive..."
 tail -f /dev/null
