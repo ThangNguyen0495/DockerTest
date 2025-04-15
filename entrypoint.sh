bbf17dc938c2e758e39cb4bcdf38937b08aa4fbd
nguyenthang <Tl291095@>
2025-04-15 09:42:53 +0700

    Customize AVD configuration

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,32 +1,44 @@
 #!/bin/bash
 
-# Start appium server by terminal
+# Start Appium server
 echo "Starting Appium..."
 appium -a 0.0.0.0 -p 4723 -pa /wd/hub --allow-cors --relaxed-security > appium_log.txt 2>&1 &
 sleep 5
 
-# Start emulator by terminal
+# Start emulator
+echo "Starting Emulator..."
 "$ANDROID_HOME"/emulator/emulator -avd emu -no-audio -no-window -gpu swiftshader_indirect -no-snapshot -no-boot-anim -verbose &
 
 # Wait for the emulator to be ready
-boot_completed=""
-while [ "$boot_completed" != "1" ]; do
-  sleep 5
-  boot_completed=$("$ANDROID_HOME"/platform-tools/adb -s emulator-5554 shell getprop sys.boot_completed || true)
-  echo "Waiting for emulator to boot..."
+echo "Waiting for Emulator to boot..."
+boot_completed=false
+while [ "$boot_completed" == false ]; do
+  sleep 10
+  boot_completed=$(adb -s emulator-5554 shell getprop sys.boot_completed 2>/dev/null)
 done
 echo "Emulator has booted!"
 
+# Disable Hidden API Policy Restrictions
 echo "Disabling Hidden API Policy Restrictions..."
-adb -P 5037 -s emulator-5554 shell settings delete global hidden_api_policy_pre_p_apps
-adb -P 5037 -s emulator-5554 shell settings delete global hidden_api_policy_p_apps
-adb -P 5037 -s emulator-5554 shell settings delete global hidden_api_policy
+adb -s emulator-5554 shell settings delete global hidden_api_policy_pre_p_apps
+adb -s emulator-5554 shell settings delete global hidden_api_policy_p_apps
+adb -s emulator-5554 shell settings delete global hidden_api_policy
 
+# Disable Animations
 echo "Disabling Animations..."
-adb -P 5037 -s emulator-5554 shell settings put global window_animation_scale 0
-adb -P 5037 -s emulator-5554 shell settings put global transition_animation_scale 0
-adb -P 5037 -s emulator-5554 shell settings put global animator_duration_scale 0
+adb -s emulator-5554 shell settings put global window_animation_scale 0
+adb -s emulator-5554 shell settings put global transition_animation_scale 0
+adb -s emulator-5554 shell settings put global animator_duration_scale 0
 
+# Download the latest APK file from GitHub
+echo "Downloading APK..."
+wget https://github.com/username/repository/releases/latest/download/app-debug.apk
+
+# Install the APK on the emulator
+echo "Installing APK..."
+adb -s emulator-5554 install app-debug.apk
+
+# List connected devices
 adb devices
 
 # Keep the container session active
