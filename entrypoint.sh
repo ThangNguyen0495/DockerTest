b005ba8176c8e9bac62f9282638be2a3b8e50cb0
nguyenthang <Tl291095@>
2025-04-17 14:07:23 +0700

    Add debug mode

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ nohup appium -a 0.0.0.0 -p 4723 -pa /wd/hub --allow-cors --relaxed-security > /d
 
 echo "[3/6] Waiting for emulator to appear in adb..."
 timeout=0
-max_wait=120
+max_wait=300
 
 while [ $timeout -lt $max_wait ]; do
   devices_output=$("$ANDROID_HOME"/platform-tools/adb  devices)
@@ -25,19 +25,21 @@ while [ $timeout -lt $max_wait ]; do
   fi
 
   echo "Waiting for emulator to appear..."
-  sleep 5
-  timeout=$((timeout + 5))
+  sleep 10
+  timeout=$((timeout + 10))
 done
 
 echo "[4/6] Waiting for emulator to complete boot..."
 boot_completed=""
 timeout=0
-max_wait=120
+max_wait=300
 while [ "$boot_completed" != "1" ] && [ $timeout -lt $max_wait ]; do
   boot_completed=$("$ANDROID_HOME"/platform-tools/adb  -s emulator-5554 shell getprop sys.boot_completed | tr -d '\r')
+  "$ANDROID_HOME"/platform-tools/adb kill-server
+  "$ANDROID_HOME"/platform-tools/adb start-server
   echo "Boot status: '$boot_completed' after ${timeout}s"
-  sleep 5
-  timeout=$((timeout + 5))
+  sleep 10
+  timeout=$((timeout + 10))
 done
 
 echo "[5/6] Disabling Hidden API Policy Restrictions..."
