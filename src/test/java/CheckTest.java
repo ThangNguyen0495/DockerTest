66355d0be4bfdbfe70c8c94141319d13604def9b
nguyenthang <Tl291095@>
2025-04-18 15:24:17 +0700

    Done android docker

--- src/test/java/CheckTest.java
+++ src/test/java/CheckTest.java
@@ -1,4 +1,3 @@
-import org.apache.logging.log4j.LogManager;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -6,21 +5,8 @@ import java.net.URISyntaxException;
 
 public class CheckTest {
     @Test
-    void test() throws IOException, URISyntaxException, InterruptedException {
-        LogManager.getLogger().info("Start emulator and appium by bash scripts");
-        ProcessBuilder pb = new ProcessBuilder("bash", "entrypoint.sh");
-        pb.inheritIO();
-        pb.start().waitFor();
-
-        LogManager.getLogger().info("Done. Start the test.");
+    void test() throws IOException, URISyntaxException {
 
         WebDriverManager.getAndroidDriver("emulator-5554", "");
     }
-
-//    @AfterTest
-//    void tearDown() throws IOException {
-//        ProcessBuilder pb = new ProcessBuilder("bash", "-c", "pkill -f emulator && pkill -f appium");
-//        pb.inheritIO();
-//        pb.start();
-//    }
 }
