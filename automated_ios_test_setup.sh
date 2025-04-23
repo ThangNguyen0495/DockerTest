0bd02b62ffb6a1a5f04a78becf050ef38699e031
nguyenthang <Tl291095@>
2025-04-23 16:20:41 +0700

    check ios setup bash scripts

--- automated_ios_test_setup.sh
+++ automated_ios_test_setup.sh
@@ -26,6 +26,7 @@ curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk install java "$JAVA_VERSION"-open
 sdk use java "$JAVA_VERSION"-open
+echo "$ANDROID_HOME"
 echo -e "${GREEN}Java $JAVA_VERSION installed via SDKMAN${NC}"
 
 echo -e "${RED_BOLD}[4/10] Installing Appium globally...${NC}"
